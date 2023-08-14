69fc29579764fa935cc5bf4d3d35b78ff4c5f863
Bhupendra Raut <bhupalraut281@gmail.com>
2023-08-14 14:49:30 +0545

    model_created

---
+++ database/migrations/2023_08_12_151328_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slogan')->nullable();
+            $table->string('adderss');
+            $table->string('tel_1');
+            $table->string('tel_2')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('youtube')->nullable();
+            $table->string('android');
+            $table->string('ios');
+            $table->string('logo');
+            $table->string('map');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
